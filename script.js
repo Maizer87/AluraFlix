c11ae7731d1bc3a46ff61d9e08f3bc254d697bdb
Maizer87 <maizer87@gmail.com>
2022-03-21 00:35:43 -0300

    criacao aluraflix

---
+++ script.js
@@ -0,0 +1,11 @@
+  var listaFilmes = [
+    "https://br.web.img2.acsta.net/pictures/19/07/23/20/57/4907896.jpg",
+    "https://m.media-amazon.com/images/I/91UusfCtQaL._AC_SL1500_.jpg",
+    "https://br.web.img3.acsta.net/medias/nmedia/18/91/90/98/20169244.jpg",
+    "https://br.web.img2.acsta.net/medias/nmedia/18/91/08/82/20128877.JPG",
+    "https://upload.wikimedia.org/wikipedia/pt/1/1d/SchindlerPoster.jpg"
+  ];
+  for (var i = 0; i < listaFilmes.length; i++) {
+    document.write("<img src=" + listaFilmes[i] + ">");
+  }
+  
